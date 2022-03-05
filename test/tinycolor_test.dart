8568731269391e11df6e5686717bfcf1d3483c99
Calvin Tam <calvintam236@users.noreply.github.com>
2022-03-05 01:15:18 -0800

    feat: rewrite test to use group

--- test/tinycolor_test.dart
+++ test/tinycolor_test.dart
@@ -1,28 +1,35 @@
-import 'dart:ui';
+import 'dart:ui' show Color;
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tinycolor2/tinycolor2.dart';
 
 void main() {
-  test(
-    "setAlpha updates alpha value of color",
+  group(
+    "TinyColor()",
     () {
       final TinyColor color = TinyColor(const Color(0xFFFFFFFF));
-      color.setAlpha(0x00);
-      expect(color.color.alpha, 0x00);
-    },
-  );
-
-  test(
-    "setOpacity updates opacity value of color",
-    () {
-      final TinyColor color =
-          TinyColor(const Color(0xFFFFFFFF).withOpacity(1.0));
-      color.setOpacity(0.5);
-
-      // underlying dart implementation converts the opacity value to an
-      // int, then back into a double. Thus some precision is loss.
-      expect(color.color.opacity, moreOrLessEquals(0.5, epsilon: 1e-2));
+      test(
+        ".setAlpha()",
+        () {
+          color.setAlpha(0x00);
+          expect(
+            color.color.alpha,
+            0x00,
+          );
+        },
+      };
+      test(
+        ".setOpacity()",
+        () {
+          color.setOpacity(0.5);
+          // underlying dart implementation converts the opacity value to an
+          // int, then back into a double. Thus some precision is loss.
+          expect(
+            color.color.opacity,
+            moreOrLessEquals(0.5, epsilon: 1e-2),
+          );
+        },
+      );
     },
   );
 }
