8bb2531003fc0e443efa2623592e0dca32ff5238
Calvin Tam <calvintam236@users.noreply.github.com>
2022-03-09 12:52:56 -0800

    feat: add remaining setters tests

--- test/tinycolor_test.dart
+++ test/tinycolor_test.dart
@@ -76,6 +76,196 @@ void main() {
         },
       );
       test(
+        ".lighten()",
+        () {
+          final TinyColor newColor = TinyColor(const Color(0xFFFFFFFF));
+          expect(
+            newColor.lighten(0).color,
+            const Color(0xFFFFFFFF),
+          );
+          expect(
+            newColor.lighten(100).color,
+            const Color(0xFFFFFFFF),
+          );
+          expect(
+            newColor.lighten().color,
+            const Color(0xFFFFFFFF),
+          );
+        },
+      );
+      test(
+        ".brighten()",
+        () {
+          final TinyColor newColor = TinyColor(const Color(0xFFFFFFFF));
+          expect(
+            newColor.brighten(0).color,
+            const Color(0xFFFFFFFF),
+          );
+          expect(
+            newColor.brighten(100).color,
+            const Color(0xFFFFFFFF),
+          );
+          expect(
+            newColor.brighten().color,
+            const Color(0xFFFFFFFF),
+          );
+        },
+      );
+      test(
+        ".darken()",
+        () {
+          final TinyColor newColor = TinyColor(const Color(0xFFFFFFFF));
+          expect(
+            newColor.darken(0).color,
+            const Color(0xFFFFFFFF),
+          );
+          expect(
+            newColor.darken(100).color,
+            const Color(0xFF000000),
+          );
+          expect(
+            newColor.darken().color,
+            const Color(0xFFE6E6E6),
+          );
+        },
+      );
+      test(
+        ".tint()",
+        () {
+          final TinyColor newColor = TinyColor(const Color(0xFFFFFFFF));
+          expect(
+            newColor.tint(0).color,
+            const Color(0xFFFFFFFF),
+          );
+          expect(
+            newColor.tint(100).color,
+            const Color(0xFFFFFFFF),
+          );
+          expect(
+            newColor.tint().color,
+            const Color(0xFFFFFFFF),
+          );
+        },
+      );
+      test(
+        ".shade()",
+        () {
+          final TinyColor newColor = TinyColor(const Color(0xFFFFFFFF));
+          expect(
+            newColor.shade(0).color,
+            const Color(0xFFFFFFFF),
+          );
+          expect(
+            newColor.shade(100).color,
+            const Color(0xFF000000),
+          );
+          expect(
+            newColor.shade().color,
+            const Color(0xFFE6E6E6),
+          );
+        },
+      );
+      test(
+        ".desaturate()",
+        () {
+          final TinyColor newColor = TinyColor(const Color(0xFFFFFFFF));
+          expect(
+            newColor.desaturate(0).color,
+            const Color(0xFFFFFFFF),
+          );
+          expect(
+            newColor.desaturate(100).color,
+            const Color(0xFFFFFFFF),
+          );
+          expect(
+            newColor.desaturate().color,
+            const Color(0xFFFFFFFF),
+          );
+        },
+      );
+      test(
+        ".saturate()",
+        () {
+          final TinyColor newColor = TinyColor(const Color(0xFFFFFFFF));
+          expect(
+            newColor.saturate(0).color,
+            const Color(0xFFFFFFFF),
+          );
+          expect(
+            newColor.saturate(100).color,
+            const Color(0xFFFFFFFF),
+          );
+          expect(
+            newColor.saturate().color,
+            const Color(0xFFFFFFFF),
+          );
+        },
+      );
+      test(
+        ".greyscale()",
+        () {
+          final TinyColor newColor = TinyColor(const Color(0xFFFFFFFF));
+          expect(
+            newColor.greyscale().color,
+            const Color(0xFFFFFFFF),
+          );
+        },
+      );
+      test(
+        ".spin()",
+        () {
+          final TinyColor newColor = TinyColor(const Color(0xFFFFFFFF));
+          expect(
+            newColor.spin(-360).color,
+            const Color(0xFFFFFFFF),
+          );
+          expect(
+            newColor.spin(-180).color,
+            const Color(0xFFFFFFFF),
+          );
+          expect(
+            newColor.spin(0).color,
+            const Color(0xFFFFFFFF),
+          );
+          expect(
+            newColor.spin(180).color,
+            const Color(0xFFFFFFFF),
+          );
+          expect(
+            newColor.spin(360).color,
+            const Color(0xFFFFFFFF),
+          );
+        },
+      );
+      test(
+        ".complement()",
+        () {
+          final TinyColor newColor = TinyColor(const Color(0xFFFFFFFF));
+          expect(
+            newColor.complement().color,
+            const Color(0xFFFFFFFF),
+          );
+        },
+      );
+      test(
+        ".mix()",
+        () {
+          final TinyColor newColor = TinyColor(const Color(0xFFFFFFFF));
+          expect(
+            newColor.mix(const Color(0xFF000000), 0).color,
+            const Color(0xFFFFFFFF),
+          );
+          expect(
+            newColor.mix(const Color(0xFF000000), 100).color,
+            const Color(0xFF000000),
+          );
+          expect(
+            newColor.mix(const Color(0xFF000000)).color,
+            const Color(0xFF808080),
+          );
+        },
+      );
+      test(
         ".clone()",
         () {
           expect(
